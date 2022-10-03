05c9aaf1ea61e2fa85d5dfec1f3101beec4c3eed
Konstantin Munteanu <konstantin@munteanu.de>
2022-10-03 10:36:34 +0200

    Cleanup mysql adapter

--- spec/support/connections.rb
+++ spec/support/connections.rb
@@ -1,5 +1,4 @@
 MssqlA = Class.new(ActiveRecord::Base)
 MssqlB = Class.new(ActiveRecord::Base)
-MysqlA = Class.new(ActiveRecord::Base)
-MysqlB = Class.new(ActiveRecord::Base)
+ModelMysql = Class.new(ActiveRecord::Base)
 ModelPostgres = Class.new(ActiveRecord::Base)
